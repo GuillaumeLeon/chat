24637c0b4b273421cce8301674b84be98fb786bc
GuillaumeOnepilot <guillaume@onepilot.co>
2025-02-15 11:33:22 +0100

    form attemp

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -1,5 +1,5 @@
 defmodule ChatWeb.RoomLive do
-  alias Chat.{Repo, Room}
+  alias Chat.{Repo, Room, Message}
 
   use ChatWeb, :live_view
 
@@ -13,19 +13,32 @@ defmodule ChatWeb.RoomLive do
     <div class="container p-2">
       <%= for message <- @room.messages do %>
         <div class="bg-blue-500 w-40 text-white p-4 rounded">
-        <p class="capitalize font-bold">{message.user.id}</p>
+        <p class="capitalize font-bold">{message.user.email}</p>
         {message.content}
         </div>
         <% end %>
+
+      <.form for={@form} phx-submit="save">
+        <.input type="text" field={@form[:message]}/>
+        <button>send</button>
+      </.form>
     </div>
     """
   end
 
-  def mount(%{"id" => id}, _, socket) do
+  def mount(%{"id" => id}, p, socket) do
 
     room = Repo.get(Room, id) 
-      |> Repo.preload(messages: [:user])
+      |> Repo.preload([messages: [:user]])
 
-    {:ok, assign(socket, :room, room)}
+    IO.inspect(p)
+    {:ok, assign(socket, %{:room => room, :form => to_form(%{})})}
+  end
+
+  def handle_event("save", %{"message" => message}, socket) do
+      current_user = socket.assigns.current_user
+    IO.inspect(message)
+    IO.inspect(current_user)
+    {:noreply, socket}
   end
 end
